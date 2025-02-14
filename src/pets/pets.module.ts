7ad518c12a25461247acda4717f4acd58a567bfb
Digital GaB <digitallgab@gmail.com>
2025-02-13 21:26:21 -0300

    feat: configura type ORM e banco de dados

---
+++ src/pets/pets.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { PetsService } from './pets.service'
+import { PetsResolver } from './pets.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Pet } from './pet.entity'
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Pet])],
+    providers: [PetsService, PetsResolver],
+    exports: [PetsService],
+})
+export class PetsModule {}
---
+++ src/pets/pets.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common'
+import { Pet } from './pet.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class PetsService {
+    constructor(
+        @InjectRepository(Pet) private petsRepository: Repository<Pet>,
+    ) {}
+    findAll(): Promise<Pet[]> {
+        return this.petsRepository.find()
+    }
+}
