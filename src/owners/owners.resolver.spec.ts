950f30e19a73780142135150c6a9ecb4a20bdeba
Digital GaB <digitallgab@gmail.com>
2025-02-15 05:28:58 -0300

    feat: cria testes para resolvers de pets e owners

--- src/owners/owners.resolver.spec.ts
+++ src/owners/owners.resolver.spec.ts
@@ -1,9 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OwnersResolver } from './owners.resolver';
 import { OwnersService } from './owners.service';
+import { Owner } from './owner.entity';
+import { CreateOwnerInput } from './dto/create-owner.input';
+import { UpdateOwnerInput } from './dto/update-owner.input';
+import { Pet } from 'src/pets/pet.entity';
 
 describe('OwnersResolver', () => {
   let resolver: OwnersResolver;
+  let service: OwnersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,9 +16,50 @@ describe('OwnersResolver', () => {
     }).compile();
 
     resolver = module.get<OwnersResolver>(OwnersResolver);
+    service = module.get<OwnersService>(OwnersService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  it('should create a new owner', async () => {
+    const createOwnerInput: CreateOwnerInput = { name: 'Gabriel Brelaz' };
+    const owner: Owner = await resolver.createOwner(createOwnerInput);
+    expect(owner).toBeDefined();
+    expect(owner.name).toBe(createOwnerInput.name);
   });
+
+  it('should retrieve all owners', async () => {
+    const owners: Owner[] = await resolver.findAll();
+    expect(owners).toBeDefined();
+    expect(owners.length).toBeGreaterThan(0);
+  });
+
+  it('should retrieve an owner by id', async () => {
+    const id: number = 1;
+    const owner: Owner = await resolver.findOne(id);
+    expect(owner).toBeDefined();
+    expect(owner.id).toBe(id);
+  });
+
+  it('should update an owner', async () => {
+    const id: number = 1;
+    const updateOwnerInput: UpdateOwnerInput = { id, name: 'Gabriel' };
+    const owner: Owner = await resolver.updateOwner(updateOwnerInput);
+    expect(owner).toBeDefined();
+    expect(owner.name).toBe(updateOwnerInput.name);
+  });
+
+  it('should retrieve pets for an owner', async () => {
+    const ownerId: number = 1;
+    const owner: Owner = await resolver.findOne(ownerId);
+    const pets: Pet[] = await resolver.pets(owner);
+    expect(pets).toBeDefined();
+    expect(pets.length).toBeGreaterThan(0);
+  });
+
+  it('should remove an owner', async () => {
+    const id: number = 1;
+    await resolver.removeOwner(id);
+    const owner: Owner = await resolver.findOne(id);
+    expect(owner).toBeNull();
+  });
+
 });
--- src/pets/pets.resolver.spec.ts
+++ src/pets/pets.resolver.spec.ts
@@ -1,18 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PetsResolver } from './pets.resolver';
+import { PetsService } from './pets.service';
+import { CreatePetInput } from './dto/create-pet.input';
+import { UpdatePetInput } from './dto/update-pet.input';
+import { Pet } from 'src/pets/pet.entity';
+import { Owner } from 'src/owners/owner.entity';
+import { CreateOwnerInput } from 'src/owners/dto/create-owner.input';
 
 describe('PetsResolver', () => {
   let resolver: PetsResolver;
+  let service: PetsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PetsResolver],
+      providers: [PetsResolver, PetsService],
     }).compile();
 
     resolver = module.get<PetsResolver>(PetsResolver);
+    service = module.get<PetsService>(PetsService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  it('should create a new pet', async () => {
+    const createPetInput: CreatePetInput = { name: 'Moana', type: 'Cat', ownerId: 1 };
+    const pet: Pet = await resolver.createPet(createPetInput);
+    expect(pet).toBeDefined();
+    expect(pet.name).toBe(createPetInput.name);
   });
+
+  it('should retrieve all Pets', async () => {
+    const pets: Pet[] = await resolver.findAll();
+    expect(pets).toBeDefined();
+    expect(pets.length).toBeGreaterThan(0);
+  });
+
+  it('should retrieve an pet by id', async () => {
+    const id: number = 1;
+    const pet: Pet = await resolver.findOne(id);
+    expect(pet).toBeDefined();
+    expect(pet.id).toBe(id);
+  });
+
+  it('should update an pet', async () => {
+    const id: number = 1;
+    const updatePetInput: UpdatePetInput = { id, name: "Sunny", type: "Dog", ownerId: 1 };
+    const pet: Pet = await resolver.updatePet(updatePetInput);
+    expect(pet).toBeDefined();
+    expect(pet.name).toBe(UpdatePetInput.name);
+  });
+
+  it('should retrieve owner for an pet', async () => {
+    const id: number = 1;
+    const pet: Pet = await resolver.findOne(id);
+    const owner: Owner = await resolver.owner(pet);
+    expect(owner).toBeDefined();
+    expect(pet.ownerId).toBe(owner.id);
+  });
+
+  it('should remove an pet', async () => {
+    const id: number = 1;
+    await resolver.removePet(id);
+    const pet: Pet = await resolver.findOne(id);
+    expect(pet).toBeNull();
+  });
+
 });
